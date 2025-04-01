eedfb6d2f78b1bd0bc6609a0a91b4a460d49a2cc
Toshinori-Ito <tito40358@gmail.com>
2025-04-02 04:17:04 +0900

    ログイン画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,23 @@
-<div class="text-center my-4">
-    <div>グーグルアカウントでログインできます</div>
+<head>
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<div class="box4">
+<div class="text-xl">グーグルアカウントでログインできます</div>
     <a href="{{ route('login.google') }}">
         <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png">
     </a>
 </div>
+<style>
+    .box4{
+    padding: 8px 19px;
+    margin: auto;
+    color: #2c2c2f;
+    background: #cde4ff;
+    border-top: solid 5px #5989cf;
+    border-bottom: solid 5px #5989cf;
+}
+.box4 p {
+    margin: 0; 
+    padding: 0;
+}
+</stle>
