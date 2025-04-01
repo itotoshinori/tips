7e2981654b5c5b025cbde6492b7b3a4681526e25
Toshinori-Ito <tito40358@gmail.com>
2025-04-01 17:03:51 +0900

    ログインページ修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center my-4">
-    <div>グーグルアカウントでログインできます。</div>
+    <div>グーグルアカウントでログインできます</div>
     <a href="{{ route('login.google') }}">
         <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png">
     </a>
